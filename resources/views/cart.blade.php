c0061676eeaa6052d0f09a3af5ec63222863f217
Akmal Mstfa <akmalmstfa29@gmail.com>
2020-07-25 12:01:44 +0700

    fix responsive cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -10,6 +10,40 @@
             margin-top:100px;
             min-height: 70vh;
         }
+		.table>tbody>tr>td, .table>tfoot>tr>td{
+			vertical-align: middle;
+		}
+		@media screen and (max-width: 600px) {
+			table#cart tbody td .form-control{
+				width:20%;
+				display: inline !important;
+			}
+			.actions .btn{
+				width:36%;
+				margin:1.5em 0;
+			}
+			
+			.actions .btn-info{
+				float:left;
+			}
+			.actions .btn-danger{
+				float:right;
+			}
+			
+			table#cart thead { display: none; }
+			table#cart tbody td { display: block; padding: .6rem; min-width:320px;}
+			table#cart tbody tr td:first-child { background: #333; color: #fff; }
+			table#cart tbody td:before {
+				content: attr(data-th); font-weight: bold;
+				display: inline-block; width: 8rem;
+			}
+			
+			
+			
+			/* table#cart tfoot td{display:block; }
+			table#cart tfoot td .btn{display:block;} */
+			
+		}
     </style>
 @endsection
 
@@ -22,13 +56,13 @@
 			<div class="col-lg-12 pl-3 pt-3">
 				<form name="cart" action="{{route('update-cart')}}" method="post">
 					@csrf
-					<table name="cart" class="table table-hover border bg-white">
+					<table id="cart" name="cart" class="table table-hover border bg-white">
 						<thead>
 							  <tr>
 								<th>Product</th>
 								<th>Price</th>
 								<th style="width:10%;">Quantity</th>
-								<th>Subtotal</th>
+								<th>Total</th>
 								<th>Action</th>
 							  </tr>
 						</thead>
@@ -47,7 +81,7 @@
 											</div>
 										</div>
 									</td>
-									<td class="align-middle" style="white-space: nowrap">
+									<td data-th="Price" class="align-middle" style="white-space: nowrap">
 										@if ($item->discount)
 											<span>
 												<del>${{$item->price_without_disc}}</del>
@@ -60,7 +94,7 @@
 										<input name="qty" data-id="{{$item->id}}" min="1" type="number" class="form-control text-center qty-form" value="{{$item->qty}}">
 										<input name="items[{{$item->id}}][qty]" type="number" class="d-none" value="{{$item->qty}}">
 									</td>
-									<td class="align-middle">
+									<td data-th="Total" class="align-middle">
 										<span>${{$item->total_price}}</span>
 										<input class="d-none" type="text" name="item_total" value="{{$item->total_price}}">
 									</td>
@@ -71,13 +105,35 @@
 							@endforeach
 						</tbody>
 						<tfoot>
-							<tr>
+							<tr class="d-md-none">
+								<td class="text-center">
+									<strong>
+										Subtotal <span></span>
+										<input class="d-none" type="text" name="total" value="">
+									</strong>
+								</td>
+							</tr>
+							<tr class="d-md-none">
+								<td>
+									<button type="submit" class="btn btn-md btn-primary btn-block w-100"  style="white-space: nowrap;">
+										<strong><i class="fa fa-angle-left"></i> Save and Continue Shopping</strong>
+									</button>
+								</td>
+							</tr>
+							<tr class="d-md-none">
+								<td>
+									<button class="btn btn-md btn-success btn-block" style="white-space: nowrap;" title="this feature is not yet available!" disabled>
+										Checkout <i class="fa fa-angle-right"></i>
+									</button>
+								</td>
+							</tr>
+							<tr class="d-none d-md-table-row">
 								<td class="align-middle">
 									<button type="submit" class="btn btn-md btn-primary btn-block w-50"  style="white-space: nowrap;">
 										<strong><i class="fa fa-angle-left"></i> Save and Continue Shopping</strong>
 									</button>
 								</td>
-								<td colspan="2" class="hidden-xs"></td>
+								<td colspan="2"></td>
 								<td class="hidden-xs align-middle" style="width:10%;">
 									<strong>
 										<span></span>
